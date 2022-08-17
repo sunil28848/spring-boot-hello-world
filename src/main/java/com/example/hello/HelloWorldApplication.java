c97641222264577edf322cccd0405d3d5785a590
sunil28848 <37667560+sunil28848@users.noreply.github.com>
2022-08-17 22:12:00 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/hello/HelloWorldApplication.java
+++ src/main/java/com/example/hello/HelloWorldApplication.java
@@ -3,7 +3,7 @@ package com.example.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
